ddd6467f12ad5396cd026b2e3239327bd9362ba4
drawix <drawixpl@gmail.com>
2021-01-05 07:30:43 +0100

    full 2021 update
    Whole rewrite, new apps, removed unncessary things from the script.
    M$ pls new LTSC monkaS

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,7 @@ $is_conemu = $env:ConEmuANSI
 
 if ($is_conemu -eq "ON") {
     Import-Module oh-my-posh
-    Set-Theme Agnoster
+    Set-Theme Robbyrussell
 }
 else {
     # $LAMBDA = ([char]955)
@@ -23,9 +23,8 @@ else {
 }
 $host.UI.RawUI.WindowTitle = $pwd
 
-Pop-Location
-Set-Alias -Name "ll" -Value "Get-ChildItem"
-function dotfiles { codium "$HOME\windowsDotfiles" }
+
+
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -34,9 +33,53 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 
+# Easier Navigation: .., ..., ...., ....., and ~
+${function:~} = { Set-Location ~\Desktop }
+${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
+${function:...} = { Set-Location ..\.. }
+${function:....} = { Set-Location ..\..\.. }
+${function:.....} = { Set-Location ..\..\..\.. }
+${function:......} = { Set-Location ..\..\..\..\.. }
 
-function .. { cd .. }
-function ... { cd .. ; cd .. }
-function .... { cd .. ; cd .. ; cd .. }
-function ..... { cd .. ; cd .. ; cd .. ; cd .. }
-function home { cd $env:USERPROFILE }
+# Navigation Shortcuts
+${function:drop} = { Set-Location ~\Documents\Dropbox }
+
+# Missing Bash aliases
+Set-Alias time Measure-Command
+
+# Correct PowerShell Aliases if tools are available (aliases win if set)
+# WGet: Use `wget.exe` if available
+if (Get-Command wget.exe -ErrorAction SilentlyContinue | Test-Path) {
+  rm alias:wget -ErrorAction SilentlyContinue
+}
+
+# Directory Listing: Use `ls.exe` if available
+if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:ls -ErrorAction SilentlyContinue
+    # Set `ls` to call `ls.exe` and always use --color
+    ${function:ls} = { ls.exe --color @args }
+    # List all files in long format
+    ${function:l} = { ls -lF @args }
+    # List all files in long format, including hidden files
+    ${function:la} = { ls -laF @args }
+    # List only directories
+    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+} else {
+    # List all files, including hidden files
+    ${function:la} = { ls -Force @args }
+    # List only directories
+    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+}
+
+# curl: Use `curl.exe` if available
+if (Get-Command curl.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:curl -ErrorAction SilentlyContinue
+    # Set `ls` to call `ls.exe` and always use --color
+    ${function:curl} = { curl.exe @args }
+    # Gzip-enabled `curl`
+    ${function:gurl} = { curl --compressed @args }
+} else {
+    # Gzip-enabled `curl`
+    ${function:gurl} = { curl -TransferEncoding GZip }
+}
+
--- WindowsPowerShell/Microsoft.VSCode_profile.ps1
+++
@@ -1,1 +0,0 @@
-Import-Module .\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
