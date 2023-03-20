366d069e73f7d4cae51c265cef6f07d9aad55f3b
Jakub <drawixpl@gmail.com>
2023-03-21 00:45:37 +0100

    Version Alpha of 2023 edition. More coming soon :heart:

---
+++ apps.ps1
@@ -0,0 +1,53 @@
+<#
+Jakub's Windows Post Install Script  (JWPIS)
+
+Author: jakub1812
+
+Requires to be ran as administrator/
+
+Using ReviOs which is mostly debloated, so deleting appx won't happend in this script
+#>
+
+
+Write-Host "check if if ran as admin"
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+Write-Host "installibf choco"
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+
+choco feature enable -n=allowGlobalConfirmation
+
+Write-Host "time for apps!"
+$appschoco = @(
+	"veracrypt",
+	"vlc",
+	"microsoft-windows-terminal",
+	"lightshot.install",
+	"qbittorrent-enhanced",
+	"firacode",
+	"eartrumpet",
+	"winaero-tweaker.install",
+	"powertoys",
+	"neovim",
+	"office-tool",
+	"firefox",
+	"au", 
+	"chocolatey-core.extension",
+	"mm-choco.extension"
+	)
+foreach ($app in $appschoco) {
+	choco install $app -y -r}
+
+	Write-Host "Winget time!"
+	Write-Host ""
+$appswinget = @(
+	"M2Team.NanaZip",
+	"TechPowerUp.NVCleanstall",
+	"Caprine.Caprine"
+)
+
+foreach ($app2 in $appswinget) {
+	winget install -e -h --accept-source-agreements --accept-package-agreements --id $app2 
+}
